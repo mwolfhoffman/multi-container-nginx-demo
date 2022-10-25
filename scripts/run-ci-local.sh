cfec1f1a0ee48b38079379f8d7db57d34789d8a4
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2022-10-25 11:06:49 -0600

    remove local travis script. added docker ignore

--- scripts/run-ci-local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash          
-
-BUILDID="build-$RANDOM"
-INSTANCE="travisci/ci-garnet:packer-1512502276-986baf0"
-docker run --name $BUILDID -dit $INSTANCE /sbin/init
-docker exec -it $BUILDID bash -l
