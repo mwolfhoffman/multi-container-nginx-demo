e6d2ce53ad830411862d6c6088a246da49349dc1
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2022-10-25 09:50:54 -0600

    d compose from 1.8.0 to 3.2

--- scripts/run-ci-local.sh
+++ scripts/run-ci-local.sh
@@ -1,5 +1,6 @@
-# https://stackoverflow.com/questions/21053657/how-to-run-travis-ci-locally
-# BUILDID="build-$RANDOM"
-# INSTANCE="travisci/ci-garnet:packer-1512502276-986baf0"
-# docker run --name $BUILDID -dit $INSTANCE /sbin/init
-# docker exec -it $BUILDID bash -l
+#!/bin/bash          
+
+BUILDID="build-$RANDOM"
+INSTANCE="travisci/ci-garnet:packer-1512502276-986baf0"
+docker run --name $BUILDID -dit $INSTANCE /sbin/init
+docker exec -it $BUILDID bash -l
