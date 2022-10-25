e158609a7113fcc7aa9a66e5dfe0edec64a6d82a
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2022-10-25 09:39:55 -0600

    trying docker-ce instead of docker-engine

---
+++ scripts/run-travis-local.sh
@@ -0,0 +1,5 @@
+# https://stackoverflow.com/questions/21053657/how-to-run-travis-ci-locally
+# BUILDID="build-$RANDOM"
+# INSTANCE="travisci/ci-garnet:packer-1512502276-986baf0"
+# docker run --name $BUILDID -dit $INSTANCE /sbin/init
+# docker exec -it $BUILDID bash -l
