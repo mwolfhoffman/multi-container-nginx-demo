95182146824142acc2a6f53c15cab1164000e7ee
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2022-10-27 07:58:30 -0600

    job lifecycle changes

--- api/core/tests.py
+++ api/core/tests.py
@@ -1,4 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
-print("Tests coming soon!")
+
+class SampleTestCase(TestCase):
+    def first_sample_test(self):
+        """More tests coming soon, for now this is to check that CI works"""
+        self.assertEqual(1,1)
